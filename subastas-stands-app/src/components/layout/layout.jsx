4389207587f2e5b2c771e0a6c8d2c14cb91691ba
= <=>
2024-03-07 21:22:31 -0600

    updated layout

--- subastas-stands-app/src/components/layout/layout.jsx
+++ subastas-stands-app/src/components/layout/layout.jsx
@@ -10,23 +10,23 @@ import {
 
 
 const navigation = [
-    { 
+    /*{ 
       name: 'Inicio', 
       href: '/'
     },
     {
       name: 'Galería', 
       href: '/galeria'
-    },
+    },*/
 ]
 
 const navigation_logged = [
 
-    {
+    /*{
       name: "Cartera Crytomonedas",
       href: '/cartera-desc',
       target: '_self'
-    }
+    }*/
 ]
 
 const footerNavigation = {
@@ -62,12 +62,6 @@ class Layout extends Component{
                         alt="logo-28wh"
                       />
                     </Link>
-                    <div className="-mr-2 flex items-center md:hidden">
-                      <Popover.Button className="focus-ring-inset inline-flex items-center justify-center rounded-md bg-principal p-2 text-white focus:outline-none focus:ring-2 focus:ring-black">
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
                   </div>
                   <div className="hidden space-x-8 md:ml-10 md:flex">
                     {navigation.map((item) => (
@@ -105,108 +99,12 @@ class Layout extends Component{
                      Salir
                    </Link>
                   : <>
-                    <Link to="/registro" className="text-base font-medium text-white">
-                      Registrarse
-                    </Link>
-                    <Link
-                      to="/login"
-                      className="inline-flex items-center rounded-md border border-transparent bg-principal px-4 py-2 text-base font-medium text-white"
-                    >
-                      Ingresar
-                    </Link>
                   </>}
                   
                 </div>
               </nav>
               <hr className='mt-4 bg-principal w-full h-2 lg:h-4' />
             </div>
-  
-            <Transition
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Popover.Panel focus className="absolute inset-x-0 top-2 origin-top transform p-2 transition md:hidden">
-                <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
-                  <div className="flex items-center justify-between px-5 pt-4">
-                    <div>
-                      <Link to='/'>
-                      <img
-                        className="h-8 w-auto"
-                        src={Logo}
-                        alt="logo-empresa"
-                      />
-                      </Link>
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
-                        <span className="sr-only">Close menu</span>
-                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                  <div className="pt-5 pb-6">
-                    <div className="space-y-1 px-2">
-                      {navigation.map((item) => (
-                        <Link
-                          key={item.name}
-                          to={item.href}
-                          className="block rounded-md px-3 py-2 text-base font-medium text-black"
-                        >
-                          {item.name}
-                        </Link>
-                      ))}
-                      {
-                        logged ? navigation_logged.map((item) => (
-                          <Link
-                            key={item.name}
-                            to={item.href}
-                            target={item.target}
-                            rel="nonreferrer noreferrer"
-                            className="block rounded-md px-3 py-2 text-base font-medium text-black"
-                          >
-                            {item.name}
-                          </Link>
-                        ))
-                        : null
-                      }
-                    </div>
-                    { logged ? <div className="mt-6 px-5">
-                      <Link
-                        to='/'
-                        onClick={logout}
-                        className="block w-full rounded-md bg-principal py-3 px-4 text-center font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700"
-                      >
-                        Salir
-                      </Link>
-                    </div> : 
-                    <>
-                    <div className="mt-6 px-5">
-                      <Link
-                        to="/registro"
-                        className="block w-full rounded-md bg-principal py-3 px-4 text-center font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700"
-                      >
-                        Registrarse
-                      </Link>
-                    </div>
-                    <div className="mt-6 px-5">
-                      <p className="text-center text-base font-medium text-gray-500">
-                        Ya tengo una cuenta.{' '}
-                        <Link to="/login" className="text-contraste hover:underline">
-                          Ingresar
-                        </Link>
-                      </p>
-                    </div>
-                    </> }
-                    
-                  </div>
-                </div>
-              </Popover.Panel>
-            </Transition>
           </Popover>
           <main className="container mx-auto sm:px-6 lg:px-8">
             {children}
