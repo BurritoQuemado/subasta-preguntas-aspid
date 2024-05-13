e90a90815b6ecbd5cd03df13e856b7cbe9acd07e
Diego Flores <dfmonroy07@gmail.com>
2024-05-13 09:20:54 -0600

    updated info

--- subastas-stands-app/src/pages/home.js
+++ subastas-stands-app/src/pages/home.js
@@ -10,17 +10,17 @@ function Home({logged_in, user_id}) {
                 <div className="flex align-center justify-center">
                     <img src={LogoAspid} alt="Logo Premios Aspid" className="h-32 md:h-52 w-auto"  />
                 </div>
-                <h2 className="text-2xl lg:text-4xl font-bold">Conferencia 22 de mayo "Twentyeight Women's Health"</h2>
+                <h2 className="text-2xl lg:text-4xl font-bold">Workshop Twentyeight women's health</h2>
                 <p className="text-md p-6 font-semibold">
-                    PREMIOS ASPID de Publicidad Iberoamericana de Salud y Farmacia 2024, en su XIX edición Auditorio Museo Soumaya - Blvd. Miguel de Cervantes Saavedra, Granada, Miguel Hidalgo, 11529, Ciudad de México, CDMX
+                Auditorio Torre Mayor -Av. P.º de la Reforma 505-Piso 51, Cuauhtémoc, 06500 Ciudad de México, CDMX,  Premios Aspid de Publicidad Iberoamericana de Salud y Farmacia 2024, en su XIX Edición
                 </p>
                 <div className="border-2 p-6 rounded border-principal">
                     <h3 className="font-bold">Workshop Twentyeight Women's Health</h3>
                     <p>
-                        Fecha 22 de mayo 2024 a las 08:30 hrs
+                        Fecha 22 de mayo 2024 a las 08:00 a 13:00 hrs
                     </p>
                     <p>
-                        Lugar: Museo Soumaya / Auditorio / Blvd. Miguel de Cervantes Saavedra, Granada, Miguel Hidalgo, 11529, Ciudad de México, CDMX
+                        Lugar: Auditorio Torre mayor Piso 51 Av. P.º de la Reforma 505-Piso 51
                     </p>
                     <p className="font-bold">
                         INSCRIPCIÓN: $2,500.00 MÁS IVA
@@ -57,13 +57,21 @@ function Home({logged_in, user_id}) {
                     En Farma Conventions S.A. de C.V. sus datos están protegidos, consulte el Aviso de Privacidad correspondiente.
                 </p>
                 <p>
-                    *Al llenar y enviar este formulario, manifiesto que conozco el aviso de privacidad y acepto que le den mis datos personales.
+                    *Al llenar y enviar este formulario, manifiesto que conozco el aviso de privacidad y acepto el tratamiento que le den a mis datos personales.
                 </p>
                 <p>
-                    Confirmaciones con Angel Bosch
+                    Confirmaciones con Mari Carmen Mendoza y/o Angel Bosch
                 </p>
                 <p>
                     Favor de requisitar y enviar por correo electrónico: &nbsp;
+                    <a
+                        href='mailto:m.mendoza@farmaindustria.com.mx'
+                        target="_blank"
+                        rel='noreferrer noopener'
+                        className='text-indigo-600'
+                    >
+                    m.mendoza@farmaindustria.com.mx
+                    </a> /
                     <a
                         href='mailto:a.bosch@farmaindustria.com.mx'
                         target="_blank"
@@ -79,25 +87,6 @@ function Home({logged_in, user_id}) {
                     Farma Conventions S.A. de C.V.
                 </p>
                 <p>
-                    <a
-                    href='mailto:inscripciones@premiosaspid.mx'
-                    target="_blank"
-                    rel='noreferrer noopener'
-                    className='text-indigo-600'
-                    >
-
-                    inscripciones@premiosaspid.mx
-                    </a>
-                    &nbsp;
-                    <a
-                        href='mailto:a.bosch@farmaindustria.com.mx'
-                        target="_blank"
-                        rel='noreferrer noopener'
-                        className='text-indigo-600'
-                    >
-                    a.bosch@farmaindustria.com.mx
-                    </a>
-                    &nbsp;
                     <a
                         href='tel:+525553537549'
                         target="_blank"
@@ -115,11 +104,16 @@ function Home({logged_in, user_id}) {
                         rel='noreferrer noopener'
                         className='text-green-600'
                     >
-                        Whatsapp: 5539439648
+                        Whatsapp: 55 3943 9648
                     </a>
                 </p>
                 <p>
-                Eje 3 Norte Calzada San Isidro No. 164 Torre Alcázar Sevilla C Oficina 1305 Col. Industrial San Antonio, Azcapotzalco, Ciudad de México C.P. 02760
+                    Eje 3 Norte Calzada San Isidro No. 164 Torre Alcázar Sevilla C Oficina 1305 Col. Industrial San Antonio, Azcapotzalco, Ciudad de México C.P. 02760
+                </p>
+                <p className="text-indigo-600">
+                    <a href="https://www.premiosaspid.mx" target="_blank" rel="noreferrer noopener">
+                        Web: www.premiosaspid.mx
+                    </a>
                 </p>
             </div>
         </div>
